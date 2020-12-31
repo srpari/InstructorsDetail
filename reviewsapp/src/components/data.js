6454b1adbbe8da04776dc4398733bc47101a5ece
Pari <pari.ramamoorthy@gmail.com>
2020-12-31 10:42:27 -0500

    Deleted Reviews

--- reviewsapp/src/components/data.js
+++ reviewsapp/src/components/data.js
@@ -1,42 +1,4 @@
-// export default reviewsData;
-export const reviewsData = [
-    {
-      id: 1,
-      name: 'Maryam Sayed',
-      ratings: 5,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      text:
-        "Absolutely love this camp! The amount of energy and creativity the teachers have is commendable! After every session, my son has something new to show and tell with huge excitement! Highly recommend it!!",
-    },
-    {
-      id: 2,
-      name: 'Vijay',
-      ratings: 5,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-      text:
-        'Chakde! Canada team, you are superb. My daughter is attending online art classes from DUBAI and really she is enjoying her classes. She is certainly improving her skills and I must mention here Raj is highly talented art tutor and his tips and tricks are surely expedient to my daughter learning. Literally I can see the great improvement on her drawing skills especially storks, outline precision, and recently she said she like the “Vector” arts.Thanks team for your dedication and profession on your field; i appreciate to keep up your work.',
-    },
-    {
-      id: 3,
-      name: 'Uma Devi',
-      ratings: 5,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-      text:
-        'My whole family loves the zumba dance program, very well organized, it is so much fun and especially during these Pandemic times it is very refreshing, especially raj and karthik are very positive and encouraging towards beginners so they always feel engrossed with the programs. My kid loved the online summer camp , this was planned meticulously with utmost patience so kids can be very well engaged in the learning process. Summer camp staff was very professional and passionately delivered the content. It was very impressive that they have chosen topics wisely age wise.',
-    },
-    {
-      id: 4,
-      name: 'Indhumathi Rao',
-      ratings: 4,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-      text:
-        'Karthik and Raj are amazing  teachers.Myself and my husband joined the adult fitness class and I would say they are best.They teach easy steps so that we can cope up with it and involve in the fitness session so easily and we find it very effective :-)',
-    },
-  ];
+// export default instructorsData;
 
   export const instructorsData =[
     {
